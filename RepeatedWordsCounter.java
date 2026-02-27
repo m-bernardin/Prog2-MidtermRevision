428c19eb15cff324120a856df8c7dd9780161769
m-bernardin <mjrb4mjrb@gmail.com>
2026-02-27 12:16:02 -0500

    no clue what i just did but it all works now yayyyyyyyyyyyyyyyyyyyyyyyyyyyyyy

--- RepeatedWordsCounter.java
+++ RepeatedWordsCounter.java
@@ -3,19 +3,32 @@ import java.util.ArrayList;
 public class RepeatedWordsCounter {
     public static String findRepeats(ArrayList<String> words){
         String answer="Input provided: ";
-        answer+=words.toString()+"\n";
+        answer+=words.toString()+"\nResults found:\n";
+        ArrayList<String> foundWords=new ArrayList<>();
         for(int i=0;i<words.size();++i){
-            answer+=words.get(i)+" ( at indexes ";
-            int count=0;
-            for(int j=0;j<words.size()-1;++j){
-                if(words.get(i).equals(words.get(j))){
-                    ++count;
-                    answer+=j+" ";
-                    words.set(j, words.get(i)+count);
+            if(!inList(foundWords,words.get(i))){
+                answer+=words.get(i)+" ( at indexes ";
+                foundWords.add(words.get(i));
+                int count=0;
+                for(int j=0;j<words.size();++j){
+                    if(words.get(i).trim().toLowerCase().equals(words.get(j).trim().toLowerCase())){
+                        ++count;
+                        answer+=j+" ";
+                    }
                 }
+                answer+="): "+count+"\n";
             }
-            answer+="): "+count+"\n";
         }
         return answer;
+    }
+    private static boolean inList(ArrayList<String> list,String toFind){
+        boolean inList=false;
+        for(String compare:list){
+            if(compare.trim().toLowerCase().equals(toFind.trim().toLowerCase())){
+                inList=true;
+                break;
+            }
+        }
+        return inList;
     }
 }
